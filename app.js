651eeca5c17db462aaec6007c4884f5ad0517675
CAR-NOVA.i <carson@carson-designs.com>
2026-02-19 20:30:49 -0800

    changed DRAG_THRESHOLD_PX from 12 to 6 to adjust grab sensitivity

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ const dragState = {
 };
 
 
-const DRAG_THRESHOLD_PX = 12; // try 8 to 14 (higher = less sensitive)
+const DRAG_THRESHOLD_PX = 6; // try 8 to 14 (higher = less sensitive)
 let pendingDrag = null;
 
 
